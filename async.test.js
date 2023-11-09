87d18ea4d2ad0214aac99ea820550879018408f6
majharul <majharuljony163@gmail.com>
2023-11-09 15:43:33 +0600

    Learn:Aync test case

---
+++ async.test.js
@@ -0,0 +1,37 @@
+import { it, expect } from "vitest";
+import { encryptMessage, encryptMessagePromise } from "./async";
+import CryptoJS from "crypto-js";
+
+it("should encrypt a message", async () => {
+  const message = {
+    name: "Mizan",
+    password: "11223344",
+  };
+
+  const secretKey = "gh#M0CL4ADe%IO59jZ83xuNv";
+
+  const encryptedMessage = await new Promise((resolve, reject) => {
+    encryptMessage(JSON.stringify(message), secretKey, (message) => {
+      resolve(message);
+    });
+  });
+  expect(encryptedMessage).toBeDefined();
+  // expect(encryptedMessage).toBe(2); // will fail in this case
+});
+
+// for promise example
+it("should encrypt the message", async () => {
+  const message = "hello world";
+  const key = "secret";
+  const encryptedMessage = await encryptMessagePromise(message, key);
+  expect(encryptedMessage).toBeDefined();
+});
+it("should encrypt the message correctly", async () => {
+  const message = "hello world";
+  const key = "secret";
+  const encryptedMessage = await encryptMessagePromise(message, key);
+  expect(encryptedMessage).toBeDefined();
+
+  const decryptedMessage = CryptoJS.AES.decrypt(encryptedMessage, key).toString(CryptoJS.enc.Utf8);
+  expect(decryptedMessage).toBe(message);
+});
